95a21f84fc03abd693b4cb0b3ddb14f7b204ee58
Heijns <>
2019-08-31 08:41:18 -0400

    Move

---
+++ projects/hyperwing/js/smartmenu.js
@@ -0,0 +1,134 @@
+( function( $ ) {
+$( document ).ready(function() {
+    var event = "mouseenter";
+    var selectors = '.smartmenu > ul > li.has-sub>a';
+    
+    function toogleMenu($this) {
+        $this.removeAttr('href');
+        var element = $this.parent('li');
+        if (element.hasClass('open')) {
+            element.removeClass('open');
+            element.find('li').removeClass('open');
+            element.find('ul').slideUp();
+        }
+        else {
+            element.addClass('open');
+            element.children('ul').slideDown();
+            element.siblings('li').children('ul').slideUp();
+            element.siblings('li').removeClass('open');
+            element.siblings('li').find('li').removeClass('open');
+            element.siblings('li').find('ul').slideUp();
+        }
+    }
+    
+    
+    if($( window ).width() < 800) {
+        event = "click";
+        selectors = '.smartmenu li.has-sub>a';
+        $(selectors).on(event, function() {
+            toogleMenu($(this));
+        });
+        
+    } else {
+        event = "mouseenter";
+        selectors = '.smartmenu > ul > li.has-sub>a';
+        $(selectors).on(event, function() {
+            toogleMenu($(this));
+        });
+        
+        $('.smartmenu  > ul').on("mouseleave", function(){
+            $(".has-sub").removeClass("open").find("ul").slideUp()
+        });
+        $('.smartmenu  > ul > ul').on("mouseleave", function(){
+            $(".has-sub").removeClass("open").find("ul").slideUp()
+        });
+        $('.smartmenu li.has-sub>a').on("click", function(){
+            toogleMenu($(this));
+        });
+    }
+    
+    $(window).resize(function (e) {
+        
+        $(".has-sub").removeClass("open").find("ul").hide()
+        
+        $('.smartmenu > ul > li.has-sub>a' ).unbind("mouseenter");
+        $('.smartmenu li.has-sub>a').unbind("click");
+        $('.smartmenu  > ul').unbind("mouseleave");
+        $('.smartmenu  > ul > ul').unbind("mouseleave");
+        
+        if($( window ).width() < 800) {
+            event = "click";
+            selectors = '.smartmenu li.has-sub>a';
+            $(selectors).on(event, function(){
+                toogleMenu($(this));
+            });
+            
+        } else {
+            event = "mouseenter";
+            selectors = '.smartmenu > ul > li.has-sub>a';
+            $(selectors).on(event, function(){
+                toogleMenu($(this));
+            });
+            $('.smartmenu  > ul').on("mouseleave", function(){
+                $(".has-sub").removeClass("open").find("ul").slideUp()
+            });
+            $('.smartmenu  > ul > ul').on("mouseleave", function(){
+                $(".has-sub").removeClass("open").find("ul").slideUp()
+            });
+            
+            $('.smartmenu li.has-sub>a').on("click", function(){
+                toogleMenu($(this));
+            });
+        }
+        
+        console.log(event);
+        
+    });
+    
+    $('.smartmenu>ul>li.has-sub>a').append('<span class="holder"></span>');
+
+    (function getColor() {
+        //return; // Temp due to bug - Remove this line and test in gastateparks.org/parks/scorp
+        var r, g, b;
+        var textColor = $('.smartmenu').css('color');
+        textColor = textColor.slice(4);
+        r = textColor.slice(0, textColor.indexOf(','));
+        textColor = textColor.slice(textColor.indexOf(' ') + 1);
+        g = textColor.slice(0, textColor.indexOf(','));
+        textColor = textColor.slice(textColor.indexOf(' ') + 1);
+        b = textColor.slice(0, textColor.indexOf(')'));
+        var l = rgbToHsl(r, g, b);
+        if (l > 0.7) {
+            //$('.smartmenu>ul>li>a').css('text-shadow', '0 1px 1px rgba(0, 0, 0, .35)');
+            $('.smartmenu>ul>li>a>span').css('border-color', 'rgba(0, 0, 0, .35)');
+        }
+        else
+        {
+            //$('.smartmenu>ul>li>a').css('text-shadow', '0 1px 0 rgba(255, 255, 255, .35)');
+            $('.smartmenu>ul>li>a>span').css('border-color', 'rgba(255, 255, 255, .35)');
+        }
+    })();
+
+    function rgbToHsl(r, g, b) {
+        r /= 255, g /= 255, b /= 255;
+        var max = Math.max(r, g, b), min = Math.min(r, g, b);
+        var h, s, l = (max + min) / 2;
+
+        if(max == min){
+            h = s = 0;
+        }
+        else {
+            var d = max - min;
+            s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+            switch(max){
+                case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+                case g: h = (b - r) / d + 2; break;
+                case b: h = (r - g) / d + 4; break;
+            }
+            h /= 6;
+        }
+        return l;
+    }
+    
+});
+} )( jQuery );
